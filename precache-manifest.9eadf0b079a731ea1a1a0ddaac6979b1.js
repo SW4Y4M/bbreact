a2faf677fd3dac7479ff14451c8dc3540cdda5e0
SW4Y4M <swayam.shashwat@gmail.com>
2022-04-14 00:28:07 +0530

    Updates

---
+++ precache-manifest.9eadf0b079a731ea1a1a0ddaac6979b1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4194914d474d3609ab707d03307ba782",
+    "url": "/bbreact/index.html"
+  },
+  {
+    "revision": "53b2b869cb48c6273b32",
+    "url": "/bbreact/static/css/main.b08f0290.chunk.css"
+  },
+  {
+    "revision": "c9413a2b0e288bcb1e85",
+    "url": "/bbreact/static/js/2.68f4301c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bbreact/static/js/2.68f4301c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53b2b869cb48c6273b32",
+    "url": "/bbreact/static/js/main.e53c350b.chunk.js"
+  },
+  {
+    "revision": "9e8e660f489cb24fdf36",
+    "url": "/bbreact/static/js/runtime-main.14c755ad.js"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "/bbreact/static/media/bg.1be2d3a0.jpg"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "/bbreact/static/media/logo.3795f66d.png"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/bbreact/static/media/spinner.235b9b3e.gif"
+  }
+]);
